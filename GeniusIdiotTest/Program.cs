37ee3b70443e638f290a162890739c3e2db78ed2
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 21:57:07 +0700

    1. Сделала функции для получения ответов пользователя 2. Добавила расчёт диагноза, который не зависит от количества вопрсов 3. Добавила сохранение результатов

--- GeniusIdiotTest/Program.cs
+++ GeniusIdiotTest/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,17 +19,16 @@ namespace GeniusIdiotTest
 
             Random random = new Random();
             Console.WriteLine("Введите имя:");
-            string userName = Console.ReadLine();
+            var userName = Console.ReadLine();
 
-            bool wantToEnd = false;
-            while (!wantToEnd)
+            while (true)
             {
                 bool[] isQuestionAsked = isQuestionAskedStartValue(questions.Length);
 
-                int countRightAnswers = 0;
-                for (int i = 0; i < diagnoses.Length; i++)
+                var countRightAnswers = 0;
+                for (int i = 0; i < 5; i++)
                 {
-                    int randomQuestionIndex = random.Next(0, questions.Length);
+                    var randomQuestionIndex = random.Next(0, questions.Length);
                     while (isQuestionAsked[randomQuestionIndex])
                     {
                         randomQuestionIndex = random.Next(0, questions.Length);
@@ -34,23 +36,7 @@ namespace GeniusIdiotTest
 
                     Console.WriteLine(questions[randomQuestionIndex]);
 
-                    int userAnswer;
-                    while (true)
-                    {
-                        try
-                        {
-                            userAnswer = Convert.ToInt32(Console.ReadLine());
-                            break;
-                        }
-                        catch (OverflowException)
-                        {
-                            Console.WriteLine("Введите число в диапозоне от -2*10**9 до 2*10**9");
-                        }
-                        catch (FormatException)
-                        {
-                            Console.WriteLine("Введите число");
-                        }
-                    }
+                    int userAnswer = GetUserAnswer();
                     isQuestionAsked[randomQuestionIndex] = true;
 
                     if (userAnswer == answers[randomQuestionIndex])
@@ -60,23 +46,19 @@ namespace GeniusIdiotTest
 
                 }
 
-                Console.WriteLine(userName + ", Вы " + diagnoses[countRightAnswers]);
+                Console.WriteLine(userName + ", Вы " + diagnoses[DiagnosesIndex(countRightAnswers, 5)]);
+                SaveUserResults(userName, diagnoses[DiagnosesIndex(countRightAnswers, 5)]);
+
+                Console.WriteLine("Хотите ли Вы посмотреть предыдущие результаты?");
+                if (GetUserChoice())
+                {
+                    ShowUserResults();
+                }
 
                 Console.WriteLine("Хотите попробовать пройти тест снова?");
-                while (true)   
+                if (!GetUserChoice())
                 {
-                    string userAnswerToContinue = Console.ReadLine().ToLower();
-                    if (userAnswerToContinue == "нет")
-                    {
-                        wantToEnd = true;
-                        break;
-                    }
-                    else if (userAnswerToContinue == "")
-                    {
-                        break;
-                    }
-
-                    Console.WriteLine("Введите да или нет");
+                    break;
                 }
                 
                 
@@ -88,6 +70,95 @@ namespace GeniusIdiotTest
 
         }
 
+        private static void ShowUserResults()
+        {
+            var value = GetValue("userResults.txt").Split('!');
+            Console.WriteLine();
+            foreach(var item in value)
+            {
+                if(item.Length == 0)
+                {
+                    break;
+                }
+                var line = item.Split('#');
+                var name = line[0];
+                var diagnose = line[1];
+                Console.WriteLine(name + " " + diagnose);
+            }
+        }
+
+        private static string GetValue(string fileName)
+        {
+            var reader = new StreamReader(fileName, Encoding.UTF8);
+            var value = reader.ReadLine();
+            reader.Close();
+            return value;
+        }
+
+        private static void SaveUserResults(string name, string diagnose)
+        {
+            var value = $"{name}#{diagnose}!";
+            AppendToFile("userResults.txt", value);
+        }
+
+        private static void AppendToFile(string fileName, string value)
+        {
+            var writer = new StreamWriter(fileName, true, Encoding.UTF8);
+            writer.Write(value);
+            writer.Close();
+        }
+
+        private static int DiagnosesIndex(int countRightAnswers, int questions)
+        {
+            int percent = countRightAnswers * 100 / questions;
+
+            if (percent < 20) { return 0; }
+            else if (percent < 40) { return 1; }
+            else if (percent < 60) { return 2; }
+            else if (percent < 80) { return 3; }
+            else if (percent < 90) { return 4; }
+            else { return 5; }
+
+        }
+
+        private static bool GetUserChoice()
+        {
+            while (true)
+            {
+                string userAnswerToContinue = Console.ReadLine().ToLower();
+                if (userAnswerToContinue == "нет")
+                {
+                    return false;
+                }
+                else if (userAnswerToContinue == "да")
+                {
+                    return true;
+                }
+
+                Console.WriteLine("Введите да или нет");
+            }
+        }
+        private static int GetUserAnswer()
+        {
+            int userAnswer;
+            while (true)
+            {
+                try
+                {
+                    userAnswer = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Введите число в диапозоне от -2*10**9 до 2*10**9");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введите число");
+                }
+            }
+            return userAnswer;
+        }
         private static bool[] isQuestionAskedStartValue(int length)
         {
             bool[] isQuestionAsked = new bool[length];
@@ -137,7 +208,7 @@ namespace GeniusIdiotTest
             questions[2] = "На двух руках десять пальцев. Сколько пальцев на пяти руках?";
             questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
             questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-            questions[4] = "Сколько месяцев в году имеют 28 дней?";
+            questions[5] = "Сколько месяцев в году имеют 28 дней?";
 
             return questions;
         }
